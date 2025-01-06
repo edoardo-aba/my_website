754d3de31d39628b03d04857ab8fe84e1c2540e8
edoardo-aba <edoardoababei2003@gmail.com>
2025-01-06 23:35:24 +0100

    setter up main

---
+++ src/components/Main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Main.css'; // Ensure you import the CSS file for styling
+
+const Main = () => {
+    return (
+        <section className='main-section'>
+            <h1>
+                Edoardo Ababei
+            </h1>
+            
+        </section>
+    );
+};
+
+export default Main;
